d7e3642d1f0e5b09e58c1296b827969661916d6f
Maurice Aarts <maurice.aarts@kpn.com>
2025-07-09 20:24:34 +0200

    Change default port from 3001 to 8080

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('App listening on port ' + port);
 });
